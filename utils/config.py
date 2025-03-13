cbf1186e20e1129a66488a982c5ac813196b41b5
luoxiurui <luoxiurui@autohome.com.cn>
2025-03-13 19:51:10 +0800

    新建项目

--- utils/config.py
+++ utils/config.py
@@ -30,8 +30,8 @@ class Project(str, Enum):
     ASSISTANT = "assistant"
     OTHER_PROJECT = "other_project"
     MARKETING = "marketing"
+    CRM_STORE_BACKEND = "crm_store_backend"
     AHOH= "ahoh"
-
 
 @singleton
 class Config(BaseSettings):
